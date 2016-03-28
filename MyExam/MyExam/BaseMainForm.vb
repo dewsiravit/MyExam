0cceca4409a9f87de22db53f9f67d12a08e5ba65
sithrawi <i.armok@yahoo.com>
2016-03-28 08:13:28 +0700

    basemainform

---
+++ MyExam/MyExam/BaseMainForm.vb
@@ -0,0 +1,6 @@
+﻿Public Class BaseMainForm
+    Private Sub LogoutLinkLabel_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LogoutLinkLabel.LinkClicked
+        LoginForm.Show()
+        Close()
+    End Sub
+End Class
