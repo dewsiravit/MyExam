e7f81a25db9bb2c8f07ac7fe472a8d2b66045911
sithrawi <i.armok@yahoo.com>
2016-03-29 07:57:28 +0700

    update create form

--- MyExam/MyExam/CreateForm.Designer.vb
+++ MyExam/MyExam/CreateForm.Designer.vb
@@ -23,6 +23,19 @@ Partial Class CreateForm
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TypeComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.PinTextBox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -60,10 +73,145 @@ Partial Class CreateForm
         Me.TypeComboBox.Size = New System.Drawing.Size(121, 28)
         Me.TypeComboBox.TabIndex = 9
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(145, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "การเข้าถึงแบบทดสอบ:"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(181, 106)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(83, 24)
+        Me.RadioButton1.TabIndex = 11
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "สาธารณะ"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(270, 106)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(120, 24)
+        Me.RadioButton2.TabIndex = 12
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "เฉพาะผู้ที่มีรหัส"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'PinTextBox
+        '
+        Me.PinTextBox.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.PinTextBox.Location = New System.Drawing.Point(396, 105)
+        Me.PinTextBox.Name = "PinTextBox"
+        Me.PinTextBox.Size = New System.Drawing.Size(254, 26)
+        Me.PinTextBox.TabIndex = 13
+        Me.PinTextBox.Text = "กำหนดรหัสเพื่อเข้าถึงแบบทดสอบ"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 146)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(165, 20)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "เวลาที่ใช้ในการทำข้อสอบ:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(430, 143)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(43, 26)
+        Me.TextBox2.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(479, 146)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 20)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "นาที"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(207, 144)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(115, 24)
+        Me.RadioButton3.TabIndex = 17
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "ไม่กำหนดเวลา"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(328, 144)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(98, 24)
+        Me.RadioButton4.TabIndex = 18
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "กำหนดเวลา"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(30, 186)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(104, 20)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "ข้อมูลผู้เข้าสอบ:"
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"ชื่อผู้ใช้", "ชื่อ", "นามสกุล", "เลขประจำตัว"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(137, 186)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(513, 109)
+        Me.CheckedListBox1.TabIndex = 20
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(137, 301)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(202, 26)
+        Me.TextBox3.TabIndex = 21
+        '
+        'Button1
+        '
+        Me.Button1.AutoSize = True
+        Me.Button1.Location = New System.Drawing.Point(345, 299)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(45, 30)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "เพิ่ม"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'CreateForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.ClientSize = New System.Drawing.Size(684, 461)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.PinTextBox)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TypeComboBox)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
@@ -74,6 +222,19 @@ Partial Class CreateForm
         Me.Controls.SetChildIndex(Me.TextBox1, 0)
         Me.Controls.SetChildIndex(Me.Label2, 0)
         Me.Controls.SetChildIndex(Me.TypeComboBox, 0)
+        Me.Controls.SetChildIndex(Me.Label3, 0)
+        Me.Controls.SetChildIndex(Me.RadioButton1, 0)
+        Me.Controls.SetChildIndex(Me.RadioButton2, 0)
+        Me.Controls.SetChildIndex(Me.PinTextBox, 0)
+        Me.Controls.SetChildIndex(Me.Label4, 0)
+        Me.Controls.SetChildIndex(Me.TextBox2, 0)
+        Me.Controls.SetChildIndex(Me.Label5, 0)
+        Me.Controls.SetChildIndex(Me.RadioButton3, 0)
+        Me.Controls.SetChildIndex(Me.RadioButton4, 0)
+        Me.Controls.SetChildIndex(Me.Label6, 0)
+        Me.Controls.SetChildIndex(Me.CheckedListBox1, 0)
+        Me.Controls.SetChildIndex(Me.TextBox3, 0)
+        Me.Controls.SetChildIndex(Me.Button1, 0)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -83,4 +244,17 @@ Partial Class CreateForm
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents TypeComboBox As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents PinTextBox As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label6 As Label
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
