88e1d4a61bd2ffcddbac0c90a87c898d3c4419af
sithrawi <i.armok@yahoo.com>
2016-03-30 01:11:11 +0700

    select combobox

--- MyExam/MyExam/CreateChoiceForm.vb
+++ MyExam/MyExam/CreateChoiceForm.vb
@@ -1,3 +1,6 @@
 ﻿Public Class CreateChoiceForm
-
+    Private Sub CreateChoiceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NumberComboBox.Items.Add(1)
+        NumberComboBox.SelectedIndex = 0
+    End Sub
 End Class
